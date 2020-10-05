3cbc170f5ae927fc73f836aa99edf134b182aad7
Maltsev Artyom <43711451+supertomix@users.noreply.github.com>
2020-10-05 22:56:49 +0300

    Add files via upload

---
+++ MotionCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class SimpleExample : MonoBehaviour
+{
+    public Transform objToFollow;
+    private Vector3 deltaPos;
+
+    void Start()
+    {
+        deltaPos = transform.position - objToFollow.position;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        transform.position = objToFollow.position + deltaPos;
+        Debug.Log("Update");
+    }
+}
+ 
---
+++ PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force;
+    public float jumpForce;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.forward * -force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * force * Time.fixedDeltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce * Time.fixedDeltaTime);
+        }
+
+    }
+}
